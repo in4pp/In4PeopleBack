40566b24931614eee7a0b3366aebb8bcf62089f4
hyeeun47113 <hyeeun47113@gmail.com>
2023-03-08 18:18:10 +0900

    #16 Personnel DTO, entity / data test

--- src/main/java/com/in4people/bootrestapi/personnel/entity/CertificateApp.java
+++ src/main/java/com/in4people/bootrestapi/personnel/entity/CertificateApp.java
@@ -1,25 +1,34 @@
 package com.in4people.bootrestapi.personnel.entity;
 
-import com.in4people.bootrestapi.personnel.dto.CertificateDTO;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-//@Entity
-//@Table(name = "CERTIFICATE_APP")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "CERTIFICATE_APP")
+@IdClass(AppPK.class)
 public class CertificateApp {
 
     // 증명서 신청
+    // 결재 FK
+    @Id
+    @Column(name = "DOC_CODE")
+    private String docCode; // 문서번호
 
-
-//    private // 결재 FK
+    @Id
+    @Column(name = "MEM_CODE")
+    private String memCode; // 사원번호
 
     @Column(name = "CER_REASON")
     private String cerReason; // 사유
 
+    @ManyToOne
     @JoinColumn(name = "CER_CODE")
-    private CertificateDTO certificateDTO; // 증명서 코드, FK, NOT NULL
+    private Certificate certificate; // 증명서 코드, FK, NOT NULL
 
 }
