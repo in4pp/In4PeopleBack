fb6e9d7356f01b1b798602e8b68931e66098d602
SeongIlCho <creba1234@gmail.com>
2023-03-08 14:45:03 +0900

    #24 approval 기본세팅완료

---
+++ src/main/java/com/in4people/bootrestapi/approval/service/ApprovalService.java
@@ -0,0 +1,19 @@
+package com.in4people.bootrestapi.approval.service;
+
+import com.in4people.bootrestapi.approval.repository.ApprovalRepository;
+import com.in4people.bootrestapi.member.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ApprovalService {
+
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+    private final ApprovalRepository approvalRepository;
+    private final ModelMapper modelMapper;
+
+}
