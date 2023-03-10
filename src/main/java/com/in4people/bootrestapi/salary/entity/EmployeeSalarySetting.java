d5f38ff65a7fd511fc8ef8962e8cca4ccede7aa7
rami2ee <sbr95887208@gmail.com>
2023-03-10 10:30:40 +0900

    #30 feat : dailyWorker_30controller,service,repository 작성 완료

--- src/main/java/com/in4people/bootrestapi/salary/entity/EmployeeSalarySetting.java
+++ src/main/java/com/in4people/bootrestapi/salary/entity/EmployeeSalarySetting.java
@@ -1,10 +1,11 @@
 package com.in4people.bootrestapi.salary.entity;
 
 import com.in4people.bootrestapi.dailyWorker.entity.DailyWorker;
+import com.in4people.bootrestapi.member.entity.Member;
 import lombok.*;
 
 import javax.persistence.*;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -21,7 +22,7 @@ public class EmployeeSalarySetting {
     // 사원번호 MEM_CODE memCode VARCHAR2(100) 사원테이블에서~ FK
 
     @Column(name = "BANK")
-    private Date bank;
+    private String bank;
 
     @Column(name = "ACCOUNT_NUMBER")
     private String accountNumber;
@@ -29,21 +30,17 @@ public class EmployeeSalarySetting {
     @Column(name = "SETTING_DATE")
     private Date settingDate;
 
-    @Column(name = "MEM_CODE")
-    private String memCode;
-
-    @OneToMany
-    @JoinColumn(name = "SALARY_SETTING_CODE")
-    private List<DailyWorker> dailyWorkerList;
+    @ManyToOne
+    @JoinColumn(name = "MEM_CODE")
+    private Member member;
 
     @Override
     public String toString() {
         return "EmployeeSalarySetting{" +
                 "salarySettingCode='" + salarySettingCode + '\'' +
-                ", bank=" + bank +
+                ", bank='" + bank + '\'' +
                 ", accountNumber='" + accountNumber + '\'' +
                 ", settingDate=" + settingDate +
-                ", memCode='" + memCode + '\'' +
                 '}';
     }
 }
